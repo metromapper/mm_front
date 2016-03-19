d71249013f273efa50ac68a84e903bb347d4d1c7
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-03-19 13:53:42 +0900

    so far

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,43 +1,47 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoia3Q4MyIsImEiOiJjaWxzdmprMncwMDdqdmRrc29mOGI2b29xIn0.CHqv97x5mK7FU5IlOwbxCA';
 
 var map = L.mapbox.map('map', 'mapbox.light')
-.setView([35.685175, 139.7528], 12);
+.setView([35.685175, 139.7528], 12)
+.addControl(L.mapbox.shareControl());
 
-var featureLayer = L.mapbox.featureLayer()
+
+
+var chiyodaLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/chiyoda.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var tozaiLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/tozai.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var nanbokuLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/nanboku.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var yurakuchoLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/yurakucho.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var ginzaLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/ginza.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
-    .loadURL('lib/geojson/hibiya.geojson')
+var hibiyaLayer = L.mapbox.featureLayer()
+    .loadURL('lib/geojson/'+'hibiya.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
-    .loadURL('lib/geojson/fukutoshin.geojson')
+var fukutoshinLayer = L.mapbox.featureLayer()
+    .loadURL('lib/geojson/'+'fukutoshin.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var marunouchiLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/marunouchi.geojson')
     .addTo(map);
 
-featureLayer = L.mapbox.featureLayer()
+var hanzoumonLayer = L.mapbox.featureLayer()
     .loadURL('lib/geojson/hanzoumon.geojson')
     .addTo(map);
 
+var features = map.featureLayer.getGeoJSON();
 
-console.log(featureLayer);
+console.log(d3.selectAll("path"));
